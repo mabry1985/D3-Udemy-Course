e61c9233a1edc041498be94e1478575bb9562d52
Josh Mabry <mabry1985@gmail.com>
2019-12-13 09:50:27 -0800

    use .enter to add more rectangles to dom based on data present

--- d3-with-data/index.js
+++ d3-with-data/index.js
@@ -1,11 +1,24 @@
 const data = [
-  {width: 200, height: 400, fill: 'red'}
+  {width: 200, height: 300, fill: 'red'},
+  {width: 20, height: 400, fill: 'blue'},
+  {width: 100, height: 40, fill: 'green'}
+
 ];
 
 const svg = d3.select('svg');
 
-const rect = svg.select('rect')
+//join data to rects
+const rects = svg.selectAll('rect')
   .data(data)
-  .attr('width', d => d.width )
+  
+//add attrs to rects already in dom 
+rects.attr('width', d => d.width )
   .attr('height', d => d.height )
   .attr('fill', d => d.fill )
+
+//add rects for additional data
+rects.enter()
+  .append('rect')
+  .attr('width', d => d.width)
+  .attr('height', d => d.height)
+  .attr('fill', d => d.fill)
