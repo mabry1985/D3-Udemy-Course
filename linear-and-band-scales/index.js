d5a6758264beab1e6d9e6b09db9896b62fab28ab
Josh Mabry <mabry1985@gmail.com>
2019-12-13 10:22:38 -0800

    add linear scale to svg

---
+++ linear-and-band-scales/index.js
@@ -0,0 +1,23 @@
+const svg = d3.select('svg');
+
+d3.json('menu.json').then(data => {
+
+  const y = d3.scaleLinear()
+    .domain([0,1000])
+    .range([0, 500]);
+
+  const rects = svg.selectAll('rect')
+    .data(data)
+
+  rects.attr('width', 50)
+    .attr('height', d => y(d.orders))
+    .attr('fill', 'orange')
+    .attr('x', (d,i) => i * 70)
+
+  rects.enter()
+    .append('rect')
+    .attr('width', 50)
+    .attr('height', d => y(d.orders))
+    .attr('fill', 'orange')
+    .attr('x', (d, i) => i * 70)
+})
