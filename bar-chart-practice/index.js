9dfa64c532ab48c9420a081137bbff3047d2c3a0
Josh Mabry <mabry1985@gmail.com>
2019-12-13 11:07:23 -0800

    add axis and invert bars

--- bar-chart-practice/index.js
+++ bar-chart-practice/index.js
@@ -13,18 +13,17 @@ const graph = svg.append('g')
   .attr('height', graphHeight)
   .attr('transform', `translate(${margin.left},${margin.top})`)
 
+const xAxisGroup = graph.append('g')
+  .attr('transform', `translate(0, ${graphHeight})`)
+const yAxisGroup = graph.append('g');
+
 d3.json('menu.json').then(data => {
 
-  const min = d3.min(data, d => d.orders);
   const max = d3.max(data, d => d.orders);
-  const extent = d3.extent(data, d => d.orders);
   
   const y = d3.scaleLinear()
     .domain([0, max])
-    .range([0,500]);
-
-
-  //returns array with min and max
+    .range([graphHeight, 0]);
 
   const x = d3.scaleBand()
     .domain(data.map( item => item.name))
@@ -36,14 +35,22 @@ d3.json('menu.json').then(data => {
     .data(data)
 
   rects.attr('width', x.bandwidth)
-    .attr('height', d => y(d.orders))
-    .attr('fill', 'orange')
-    .attr('x', d => x(d.name));
+    .attr('height', d => graphHeight - y(d.orders))
+    .attr('fill', 'blue')
+    .attr('x', d => x(d.name))
+    .attr('y', d => y(d.orders));
 
   rects.enter()
     .append('rect')
     .attr('width', x.bandwidth)
-    .attr('height', d => y(d.orders))
-    .attr('fill', 'orange')
-    .attr('x', d => x(d.name));
+    .attr('height', d => graphHeight - y(d.orders))
+    .attr('fill', 'blue')
+    .attr('x', d => x(d.name))
+    .attr('y', d => y(d.orders));
+
+  //create and call axes
+  const xAxis = d3.axisBottom(x);
+  const yAxis = d3.axisLeft(y);
+  xAxisGroup.call(xAxis);
+  yAxisGroup.call(yAxis);
 })
