86370e300f51103a303130f7f4e3a267f00bbde3
Josh Mabry <mabry1985@gmail.com>
2019-12-13 09:32:54 -0800

    create dummy data and functions to pull data into svgs

---
+++ d3-with-data/index.js
@@ -0,0 +1,11 @@
+const data = [
+  {width: 200, height: 400, fill: 'red'}
+];
+
+const svg = d3.select('svg');
+
+const rect = svg.select('rect')
+  .data(data)
+  .attr('width', function(d, i, n){ return d.width })
+  .attr('height', function(d, i, n){ return d.height })
+  .attr('fill', function(d, i, n){ return d.fill })
