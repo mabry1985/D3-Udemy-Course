d57c1f159afb08544930f486b9b3a8975435f2fb
Josh Mabry <mabry1985@gmail.com>
2019-12-13 10:31:44 -0800

    add band scales and padding

--- linear-and-band-scales/index.js
+++ linear-and-band-scales/index.js
@@ -4,20 +4,26 @@ d3.json('menu.json').then(data => {
 
   const y = d3.scaleLinear()
     .domain([0,1000])
-    .range([0, 500]);
+    .range([0,500]);
+
+  const x = d3.scaleBand()
+    .domain(data.map( item => item.name))
+    .range([0,500])
+    .paddingInner(0.2)
+    .paddingOuter(0.2);
 
   const rects = svg.selectAll('rect')
     .data(data)
 
-  rects.attr('width', 50)
+  rects.attr('width', x.bandwidth)
     .attr('height', d => y(d.orders))
     .attr('fill', 'orange')
-    .attr('x', (d,i) => i * 70)
+    .attr('x', d => x(d.name));
 
   rects.enter()
     .append('rect')
-    .attr('width', 50)
+    .attr('width', x.bandwidth)
     .attr('height', d => y(d.orders))
     .attr('fill', 'orange')
-    .attr('x', (d, i) => i * 70)
+    .attr('x', d => x(d.name));
 })
