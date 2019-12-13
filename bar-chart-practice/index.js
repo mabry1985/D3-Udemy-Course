c8414eab7729a1a30f3e331eaced449fa0b608d5
Josh Mabry <mabry1985@gmail.com>
2019-12-13 11:12:42 -0800

    transform text

--- bar-chart-practice/index.js
+++ bar-chart-practice/index.js
@@ -50,7 +50,16 @@ d3.json('menu.json').then(data => {
 
   //create and call axes
   const xAxis = d3.axisBottom(x);
-  const yAxis = d3.axisLeft(y);
+  const yAxis = d3.axisLeft(y)
+    .ticks(3)
+    .tickFormat(d => d + ' orders');
+
   xAxisGroup.call(xAxis);
   yAxisGroup.call(yAxis);
+
+  xAxisGroup.selectAll('text')
+    .attr('transform', 'rotate(-40)')
+    .attr('text-anchor', 'end')
+    .attr('font-size', '14px')
+    .attr('color', 'green')
 })
