90a7737e918291dd07657481999cb922aaac9221
Josh Mabry <mabry1985@gmail.com>
2019-12-13 16:33:35 -0800

    add firebase and subscribe to database, add transitions for rect

--- bar-chart-practice/index.js
+++ bar-chart-practice/index.js
@@ -7,59 +7,102 @@ const svg = d3.select('.canvas')
 const margin = {top: 20, right: 20, bottom: 100, left: 100};
 const graphWidth = 600 - margin.left - margin.right;
 const graphHeight = 600 - margin.top - margin.bottom;
-
+  
 const graph = svg.append('g')
   .attr('width', graphWidth)
   .attr('height', graphHeight)
   .attr('transform', `translate(${margin.left},${margin.top})`)
-
+  
 const xAxisGroup = graph.append('g')
   .attr('transform', `translate(0, ${graphHeight})`)
+
+xAxisGroup.selectAll('text')
+  .attr('transform', 'rotate(-40)')
+  .attr('text-anchor', 'end')
+  .attr('font-size', '14px')
+
 const yAxisGroup = graph.append('g');
 
-d3.json('menu.json').then(data => {
+const y = d3.scaleLinear()
+  .range([graphHeight, 0]);
+const x = d3.scaleBand()
+  .range([0, graphWidth])
+  .paddingInner(0.2)
+  .paddingOuter(0.2);
 
-  const max = d3.max(data, d => d.orders);
+// create & call axes
+const xAxis = d3.axisBottom(x);
+const yAxis = d3.axisLeft(y)
+  .ticks(3)
+  .tickFormat(d => d + ' orders');
   
-  const y = d3.scaleLinear()
-    .domain([0, max])
-    .range([graphHeight, 0]);
-
-  const x = d3.scaleBand()
-    .domain(data.map( item => item.name))
-    .range([0,500])
-    .paddingInner(0.2)
-    .paddingOuter(0.2);
-
+const update = (data) => {
+  //updating scale domains
+  y.domain([0,d3.max(data, d => d.mentions)]);
+  x.domain(data.map(item => item.name));
+    
+  //join the data to rects
   const rects = graph.selectAll('rect')
-    .data(data)
-
+  .data(data);
+  
+  //remove unwanted shapes
+  rects.exit().remove();
+  
+  //update current shapes in DOM
   rects.attr('width', x.bandwidth)
-    .attr('height', d => graphHeight - y(d.orders))
-    .attr('fill', 'blue')
+    .attr('fill', 'orange')
     .attr('x', d => x(d.name))
-    .attr('y', d => y(d.orders));
+    .transition().duration(750)
+    .attr("height", d => graphHeight - y(d.mentions))
+    .attr('y', d => y(d.mentions));
 
   rects.enter()
     .append('rect')
-    .attr('width', x.bandwidth)
-    .attr('height', d => graphHeight - y(d.orders))
-    .attr('fill', 'blue')
-    .attr('x', d => x(d.name))
-    .attr('y', d => y(d.orders));
+      .attr('width', x.bandwidth)
+      .attr('height', 0)
+      .attr('fill', 'blue')
+      .attr('x', d => x(d.name))
+      .attr('y', graphHeight)
+      .transition().duration(750)
+      .attr("height", d => graphHeight - y(d.mentions))
+      .attr('y', d => y(d.mentions));
 
-  //create and call axes
-  const xAxis = d3.axisBottom(x);
-  const yAxis = d3.axisLeft(y)
-    .ticks(3)
-    .tickFormat(d => d + ' orders');
+    xAxisGroup.call(xAxis);
+    yAxisGroup.call(yAxis);
+};
 
-  xAxisGroup.call(xAxis);
-  yAxisGroup.call(yAxis);
+let data = [];
 
-  xAxisGroup.selectAll('text')
-    .attr('transform', 'rotate(-40)')
-    .attr('text-anchor', 'end')
-    .attr('font-size', '14px')
-    .attr('color', 'green')
+db.collection('frameworks').onSnapshot(res => {
+  
+  res.docChanges().forEach(change => {
+    const doc = {...change.doc.data(), id: change.doc.id};
+    
+    switch(change.type) {
+      case 'added':
+        data.push(doc);
+        break;
+      case 'modified':
+        const index = data.findIndex(item => item.id == doc.id)
+        data[index] = doc;
+        break;
+      case 'removed':
+        data = data.filter(item => item.id !== doc.id);
+        break;
+      default:
+        return;
+    }
+    
+    update(data);
+  })
+
+  update(data)
 })
+
+
+
+
+
+
+
+
